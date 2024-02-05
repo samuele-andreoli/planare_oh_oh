8564e18cf9e24bfbf3a3531ae777496d147678b8
enripicc <94044209+enripicc@users.noreply.github.com>
2024-02-05 12:12:54 +0100

    Update tests.m

--- tests.m
+++ tests.m
@@ -1,11 +1,13 @@
 load "lib/FamiliesPlanar.m";
 load "lib/planar.m";
-n:=4;
-p:=3;
-F:=GF(p^n);
-R:=PolynomialRing(F);
-Fam:=getAllDOPlanar(R);
-FFs:=getFFs(F);
-for f in Fam do
-  fastIsPlanarDOPoly(f);
+for n:=2 to 8 do
+  printf "test dimension %o\n",n;
+  p:=3;
+  F:=GF(p^n);
+  R:=PolynomialRing(F);
+  Fam:=getAllDOPlanar(R);
+  FFs:=getFFs(F);
+  for f in Fam do
+    if not fastIsPlanarDOPoly(f,FFs) then error""; end if;
+  end for;
 end for;
