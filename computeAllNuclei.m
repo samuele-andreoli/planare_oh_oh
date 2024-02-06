b19bfd871184a580381113f70fddeb9f8e865507
enripicc <94044209+enripicc@users.noreply.github.com>
2024-02-06 11:47:36 +0100

    Create computeAllNuclei.m

---
+++ computeAllNuclei.m
@@ -0,0 +1,19 @@
+load "representatives.m";
+load "lib/semifields.m";
+load "lib/planar.m";
+
+
+for n:=3 to 8 do
+  Fun:=getFun(n);
+  F:=BaseRing(Parent(Fun[1]));
+  e:=One(F);
+  subfields,sizes:=PrecomputeSubfields(F);
+  for i:=1 to #Fun do
+    printf "%o.%o\t",n,i;
+    if isDOPolynomial(Fun[i]) then
+      N:=Nuclei(Fun[i], e,subfields,sizes);
+      printf "%o",N;
+    end if;
+    printf "\n\n";
+  end for;
+end for;
