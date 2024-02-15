57a27583da511989fb270b6085922d34b0628664
enripicc <94044209+enripicc@users.noreply.github.com>
2024-02-15 15:23:37 +0100

    Update isotopic_classification.m

--- isotopic_classification.m
+++ isotopic_classification.m
@@ -2,43 +2,50 @@ load "lib/representatives.m";
 load "lib/dupeq.m";
 load "lib/semifields.m";
 load "lib/planar.m";
+load "lib/invariantTable.m";
+
+
 for n:=2 to 8 do
+	OrbitsTable:=getOrbitsTable(n);
   F<a>:=GF(3^n);
   R<x>:=PolynomialRing(F);
   Fun:=PowerSequence(R)!getRepresentatives(n);
-  e:=One(F);
+  E:={u: u in F|not IsZero(u)};
   for i:=1 to #Fun do
     f:=Fun[i];
     printf "\n%o.%o\t",n,i;
     star:=function(u,v)
         return Evaluate(f,u+v) - Evaluate(f,u) - Evaluate(f,v);
     end function;
-
-    starPsi := Interpolation([star(u,e): u in F],[u: u in F]);
-
-    asterisk := function(u,v)
-        return star(Evaluate(starPsi,u),Evaluate(starPsi,v));
-    end function;
     if isDOPolynomial(f) then
-		  N,Nm:=getNuclei(f,e);
-		  if IsEven(Floor(Log(3,#Nm)/Log(3,#N))) then
-		    printf"CH candidate\t";
-		    bol:=true;
-		    for b in Nm diff N do
-		      f1:=Interpolation([u: u in F],[asterisk(asterisk(b,u),u): u in F]);
-		      //if not isPlanar(f1) then error""; end if;
-		      if not dupeq(f,f1) then
-		        printf "splits\t";
-		        if not isPlanar(f1) then error""; end if;
-		        f1;
-		        bol:=false;
-		        break;
-		      end if;
-		    end for;
-		    if bol then
-		      printf "does not split";
-		    end if;
-		  end if;
-    end if;
+			CardN:=Nuclei(f,One(F));
+			if IsEven(Floor(Log(3,CardN[2])/Log(3,CardN[1]))) then
+				printf"CH candidate\t";
+				flag:=false;
+				for e in E do
+					starPsi := Interpolation([star(u,e): u in F],[u: u in F]);
+
+					asterisk := function(u,v)
+						  return star(Evaluate(starPsi,u),Evaluate(starPsi,v));
+					end function;
+					N,Nm:=getNuclei(f,e);
+					for b in Nm diff N do
+						f1:=Interpolation([u: u in F],[asterisk(asterisk(b,u),u): u in F]);
+						//the second dupeq is to double check
+						if not dupeq_with_l2_representatives(f,f1,OrbitsTable[f]) and not dupeq(f,f1) then
+						  printf "splits\t";
+						  if not isPlanar(f1) then error""; end if;
+						  f1;
+						  flag:=true;
+						  break;
+						end if;
+					end for;
+					if flag then break; end if;
+				end for;
+				if not flag then
+					printf "does not split";
+				end if;
+			end if;
+		end if;
   end for;
 end for;
