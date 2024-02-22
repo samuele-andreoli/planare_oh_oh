b2450cbd152ef59f920007bfce881f39d32900a8
enripicc <94044209+enripicc@users.noreply.github.com>
2024-02-22 12:28:13 +0100

    Update reduce_split.m still to do listIntersectionsFam

--- reduce_split.m
+++ reduce_split.m
@@ -1,6 +1,8 @@
 load "lib/dupeq.m";
 load "lib/semifields.m";
 load "lib/planar.m";
+load "lib/FamiliesPlanar.m";
+load "lib/representatives.m";
 
 isMonomial:=function(f)
   return #SequenceToSet(Coefficients(f)) eq 2;
@@ -72,7 +74,7 @@ ReduceFun:=procedure(~Fun:NucleiFun:=AssociativeArray(),OrbitsFun:=AssociativeAr
   end if;
 end procedure;
 
-SplitFun:=procedure(~Fun:NucleiFun:=AssociativeArray(),OrbitsFun:=AssociativeArray())
+SplitFun:=procedure(~Fun,~NucleiFun,OrbitsFun:=AssociativeArray())
 
   if IsEmpty(Fun) then
     return;
@@ -89,10 +91,18 @@ SplitFun:=procedure(~Fun:NucleiFun:=AssociativeArray(),OrbitsFun:=AssociativeArr
     end if;
   end for;
 
-  myIsEquivalent:=function(f,g)
-    if IsDefined(OrbitsFun,f) then
+  testEquivalence:=function(f,g)
+    iObol:=IsDefined(OrbitsFun,f);
+    jObol:=IsDefined(OrbitsFun,g);
+    Nbol:=IsDefined(NucleiFun,f) and IsDefined(NucleiFun,g);
+    Abol:=IsDefined(AutomorphismFun,f) and IsDefined(AutomorphismFun,g);
+    if Nbol and [#NN:NN in NucleiFun[f]] ne [#NN:NN in NucleiFun[g]] then
+      return false;
+    elif Abol and AutomorphismFun[f] ne AutomorphismFun[g] then
+      return false;
+    elif iObol then
       return dupeq_with_l2_representatives(f,g,OrbitsFun[f]);
-    elif IsDefined(OrbitsFun,g) then
+    elif jObol then
       return dupeq_with_l2_representatives(g,f,OrbitsFun[g]);
     else
       if isMonomial(f) then
@@ -103,6 +113,7 @@ SplitFun:=procedure(~Fun:NucleiFun:=AssociativeArray(),OrbitsFun:=AssociativeArr
         return dupeq(g,f);
       end if;
     end if;
+    error "";
   end function;
 
   getNewSplit:=function(f)
@@ -119,7 +130,7 @@ SplitFun:=procedure(~Fun:NucleiFun:=AssociativeArray(),OrbitsFun:=AssociativeArr
     CandidatesB:={Rep({asterisk(u,b): u in N|not IsZero(u)}): b in (Nm diff N)};
     for b in CandidatesB do
       f1:=Interpolation([u: u in F],[asterisk(asterisk(b,u),u): u in F]);
-      if not myIsEquivalent(f,f1) then
+      if not testEquivalence(f,f1) then
         return f1;
       end if;
     end for;
@@ -131,6 +142,7 @@ SplitFun:=procedure(~Fun:NucleiFun:=AssociativeArray(),OrbitsFun:=AssociativeArr
     if IsEven(Floor(Log(p,#NucleiFun[f][2])/Log(p,#NucleiFun[f][1]))) then
       f0:=getNewSplit(f);
       Append(~NewFun,f0);
+      NucleiFun[f0]:=NucleiFun[f];
     else
       Append(~NewFun,f);
     end if;
@@ -140,8 +152,7 @@ SplitFun:=procedure(~Fun:NucleiFun:=AssociativeArray(),OrbitsFun:=AssociativeArr
     if not Fun[i] eq NewFun[i] then
       bolNew:=true;
       for j in Exclude([1..card],i) do
-        if ([#NN:NN in NucleiFun[Fun[i]]] eq [#NN:NN in NucleiFun[Fun[j]]]) and myIsEquivalent(Fun[j],NewFun[i]) then
-          Remove(~Fun,i);
+        if testEquivalence(Fun[j],NewFun[i]) then
           bolNew:=false;
           break;
         end if;
@@ -154,7 +165,8 @@ SplitFun:=procedure(~Fun:NucleiFun:=AssociativeArray(),OrbitsFun:=AssociativeArr
 end procedure;
 
 //I assume that Funs have already been in the previous steps
-listIntersectionsFam:=procedure(Funs, StrFam :NucleiFun:=AssociativeArray(),OrbitsFun:=AssociativeArray(),AutomorphismFun:=AssociativeArray())
+listIntersectionsFam:=function(Funs, StrFam :NucleiFun:=AssociativeArray(),OrbitsFun:=AssociativeArray(),AutomorphismFun:=AssociativeArray())
+  allFun:=[];
   FamList:=AssociativeArray();
   for i:=1 to #Funs do
     for f in Funs[i] do
@@ -190,6 +202,7 @@ listIntersectionsFam:=procedure(Funs, StrFam :NucleiFun:=AssociativeArray(),Orbi
       for j:=(i+1) to #Funs do
         for g in Funs[j] do
           if testEquivalence(f,g) then
+            Exclude(~Funs[j]
             NewFams:=FamList[f] cat FamList[g];
             for fam in NewFams do
               Include(~FamList[f],fam);
@@ -200,7 +213,8 @@ listIntersectionsFam:=procedure(Funs, StrFam :NucleiFun:=AssociativeArray(),Orbi
       end for;
     end for;
   end for;
-end procedure;
+  return FamList;
+end function;
 
 
 ReducePolyForm:=function(f)
@@ -209,42 +223,48 @@ ReducePolyForm:=function(f)
   return R!Interpolation([u: u in F],[Evaluate(f,u): u in F]);
 end function;
 
-load "lib/FamiliesPlanar.m";
-n:=6;
-F<a>:=GF(3^n);
-R<x>:=PolynomialRing(F);
-Funs:=[fun(R): fun in [getG,getZP,getCG,getD,getBH,getB,getZKW,getCMDY,getA,getFF]];
-Funs:=[[ReducePolyForm(f): f in Fun]: Fun in Funs];
-myNuclei:=AssociativeArray();
-myOrbits:=AssociativeArray();
-StrFam:=["G","ZP","CG","D","BH","B","ZKW","CMDY","A","FF"];
-for i:=1 to #Funs do
-  printf "\n\n\n\n---------\nFamily %o\n\n",StrFam[i];
-  printf "removing monomials...";
-  RemovePower(~Funs[i]);
-  printf "done\n";
-  printf "computing invariants...";
-  for f in Funs[i] do
+
+ClassifyFun:=procedure(n)
+  F<a>:=GF(3^n);
+  R<x>:=PolynomialRing(F);
+  Funs:=[fun(R): fun in [getG,getZP,getCG,getD,getBH,getB,getZKW,getCMDY,getA,getFF]];
+  Funs:=[[ReducePolyForm(f): f in Fun]: Fun in Funs];
+  myNuclei:=AssociativeArray();
+  myOrbits:=AssociativeArray();
+  myAuthomorphisms:=AssociativeArray();
+  StrFam:=["G","ZP","CG","D","BH","B","ZKW","CMDY","A","FF"];
+  for i:=1 to #Funs do
+    printf "\n\n\n\n---------\nFamily %o\n\n",StrFam[i];
+    printf "removing monomials...";
+    RemovePower(~Funs[i]);
+    printf "done\n";
+    printf "computing invariants...";
+    for f in Funs[i] do
+      if not IsDefined(myNuclei,f) then
+        myNuclei[f]:=getNuclei(f,One(F));
+      end if;
+    end for;
+    printf "done\n";
+    printf "reducing functions...";
+    ReduceFun(~Funs[i]:NucleiFun:=myNuclei,OrbitsFun:=myOrbits,AuthomorphismsFun:=myAuthomorphisms);
+    printf "done\n";
+    printf "splitting functions...";
+    SplitFun(~Funs[i],~myNuclei:OrbitsFun:=myOrbit,AuthomorphismsFun:=myAuthomorphisms);
+    printf "done\n";
+    printf "\nNumber of classes %o",#Funs[i];
+  end for;
+  printf "completing invariants..";
+  for f in &cat(Funs) do
     if not IsDefined(myNuclei,f) then
       myNuclei[f]:=getNuclei(f,One(F));
     end if;
-    //myOrbits[f]:=partitionByL2(f);
   end for;
   printf "done\n";
-  printf "reducing functions...";
-  ReduceFun(~Funs[i]:NucleiFun:=myNuclei,OrbitsFun:=myOrbits);
-  printf "done\n";
-  printf "splitting functions...";
-  SplitFun(~Funs[i]:NucleiFun:=myNuclei,OrbitsFun:=myOrbits);
-  printf "done\n";
-  printf "\nNumber of classes %o",#Funs[i];
-end for;
-printf "completing invariants..";
-for f in &cat(Funs) do
-  if not IsDefined(myNuclei,f) then
-    myNuclei[f]:=getNuclei(f,One(F));
-  end if;
-end for;
-printf "done\n";
-printf "\n\n\n\n---------\nCheck intersections between families\n\n";
-listIntersectionsFam(Funs, StrFam :NucleiFun:=myNuclei,OrbitsFun:=myOrbits);
+  printf "\n\n\n\n---------\nCheck intersections between families\n\n";
+  FamList:=listIntersectionsFam(Funs, StrFam :NucleiFun:=myNuclei,OrbitsFun:=myOrbits,AuthomorphismsFun:=myAuthomorphisms);
+  
+  myRep:=PowerSequence(R)!getRepresentatives(n);
+  for f in myRep do
+    for g in Keys(FamList)
+  
+end procedure;
