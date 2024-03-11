4e71d0c521b1a2003b17432d166544dfc60a7bba
enripicc <94044209+enripicc@users.noreply.github.com>
2024-03-11 15:05:33 +0100

    Update semifields.m

--- lib/semifields.m
+++ lib/semifields.m
@@ -27,60 +27,6 @@ DOToSemifieldPoly:=function(f, e)
     return asterisk(a,b);
 end function;
 
-Nuclei:=function(f, e)
-    assert not IsZero(e);
-
-    F := Parent(e);
-
-    RR<a,b,c> := PolynomialRing(F,3);
-    R0 := PolynomialRing(RR);
-
-    f := R0!f;
-    FE := [a^(#F)-a,b^(#F)-b,c^(#F)-c];
-
-    // Fast evaluation of a polynomial and reduction by the field equations.
-    EvaluateMod := function(f0,u)
-        return &+[NormalForm(Evaluate(t,u), FE) : t in Terms(f0)];
-    end function;
-
-    // Construct polynomials for the star and * products
-    star:=function(u,v)
-        return EvaluateMod(f,u+v) - EvaluateMod(f,u) - EvaluateMod(f,v);
-    end function;
-
-    starPsi := R0!Interpolation([F!star(u,e): u in F],[u: u in F]);
-
-    asterisk := function(u,v)
-        return star(EvaluateMod(starPsi,u),EvaluateMod(starPsi,v));
-    end function;
-
-    // Associativity equation
-    fl := asterisk(asterisk(a,b),c);
-    fr := Evaluate(fl,[b,c,a]);
-    g  := fl-fr;
-
-    // Max order of a non trivial (Fq) nucleus
-    p:=Characteristic(F);
-    D:=Divisors(Degree(F));
-    MaxOrder:=p^D[#D-1]+1;
-
-    mn:=0;
-    rn:=0;
-    for u in F do
-        if mn eq MaxOrder then
-            return [#F,#F];
-        end if;
-
-        if IsZero(Evaluate(g,[a,b,u])) then
-            rn +:=1;
-            mn +:=1;
-        elif IsZero(Evaluate(g,[a,u,b])) then
-            mn +:=1;
-        end if;
-    end for;
-
-    return [rn,mn];
-end function;
 
 getNuclei:=function(f, e)
     assert not IsZero(e);
@@ -117,19 +63,29 @@ getNuclei:=function(f, e)
     // Max order of a non trivial (Fq) nucleus
     p:=Characteristic(F);
     D:=Divisors(Degree(F));
-    Nm:={};
-    N:={};
-
-    mn:=0;
-    rn:=0;
+    Nm:={id*a: a in PrimeField(F)};
+    N:=Nm;
     for u in F do
-        if IsZero(Evaluate(g,[a,b,u])) then
-            Include(~N,u);
-            Include(~Nm,u);
-        elif IsZero(Evaluate(g,[a,u,b])) then
-            Include(~Nm,u);
+        if not u in N and IsZero(Evaluate(g,[a,b,u])) then
+            u0:=u;
+            while not u0 eq id do
+                Include(~Nm,u0);
+                Include(~N,u0);
+                u0:=asterisk(u,u0);
+            end while;
+        elif not u in Nm and IsZero(Evaluate(g,[a,u,b])) then
+            u0:=u;
+            while not u0 eq id do
+                Include(~Nm,u0);
+                u0:=asterisk(u,u0);
+            end while;
         end if;
     end for;
-
     return [N,Nm];
+end function;
+
+
+Nuclei:=function(f, e)
+    NN:=getNuclei(f,e);
+    return [#NN[1],#NN[2]];
 end function;
